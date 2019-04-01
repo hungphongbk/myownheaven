c3a500ef78f079436f55631a002f839c1a3b7168
myowngrave <myowngrave1218@gmail.com>
2019-04-01 12:14:15 +0700

    2nd edit

--- src/components/bio.js
+++ src/components/bio.js
@@ -27,10 +27,6 @@ function Bio() {
             <p>
               Written by <strong>{author}</strong> who lives and works in San
               Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
             </p>
           </div>
         )
@@ -52,7 +48,6 @@ const bioQuery = graphql`
       siteMetadata {
         author
         social {
-          twitter
         }
       }
     }
